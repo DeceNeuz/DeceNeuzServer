9a0d234d083753136ab69f4bf7056b65b6458002
Ayush Dey <deyayush6@gmail.com>
2024-02-03 17:02:45 +0530

    remove-env 4

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"github.com/SubhranilRaha/go-crud/controllers"
 	"github.com/SubhranilRaha/go-crud/initializers"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func init(){
@@ -19,5 +20,19 @@ func main(){
 	r.POST("/login", controllers.LoginHandler)
 	r.PATCH("/likes/:id", controllers.LikesUpdate)
 	// PORT := 3000
-	r.Run("localhost:3000")
+	// r.Run("localhost:3000")
+
+	server := &http.Server{
+		Addr:         ":3000", // Set the desired port
+		Handler:      r,
+		/* ReadTimeout:  15 * time.Second, // Adjust these values based on your application's needs
+		WriteTimeout: 15 * time.Second,
+		IdleTimeout:  60 * time.Second, */
+	}
+
+	// Start the server
+	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		// Handle any errors that occur during server startup
+		panic(err)
+	}
 }
